78f15acdddc3f6fe2356495e82469051fe1ce329
Principle <qq357478904@gmail.com>
2014-08-16 07:45:03 +0800

    aaa
    aaa

--- lisp.el
+++ lisp.el
@@ -17,4 +17,28 @@ flowers
       herbivores '(gazelle antelope zebra))
 (setq counter 0)       ;let's initialize
 (setq counter (+ counter 1)) 
-counter 
+(buffer-name)
+(buffer-file-name)"h:/Lisp/lisp.el"
+
+(current-buffer)
+(other-buffer)
+(switch-to-buffer (other-buffer))
+(buffer-size)
+
+(defun multiply-by-seven (number)
+  "Multiply NUMBER by seven."
+  (* 7 number))
+(multiply-by-seven 3)
+
+(defun multiply-by-seven (number)
+  "Multiply num by seven."               ;second version
+  (+ number number number number number number number))
+(multiply-by-seven 7)
+
+(defun multiply-by-seven (number)
+					; Interactive version.
+  "Multiply NUMBER by seven."
+  (interactive "p")
+  (message "The result is %d" (* 7 number)))
+
+
